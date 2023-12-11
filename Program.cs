0e11d17d178f19d42de96b1b784dfcb0477b030b
Erkan Mert <erkanmertkilavuz@gmail.com>
2023-12-11 10:44:20 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProgTemelleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+        }
+    }
+}
