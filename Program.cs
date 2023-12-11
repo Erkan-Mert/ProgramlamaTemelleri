6b7556c871ef31dc4d0de2515edbb09ae68d4d61
Erkan Mert <erkanmertkilavuz@gmail.com>
2023-12-11 11:58:32 +0300

    Veri Tipleri Ders

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
 ﻿namespace ProgTemelleri
 {
-    internal class Program
+    class Program
     {
+        //Stack
+        //Heap
+        //Register
+        //--------------------------------
+        //Değişken Burada(Sınıf içinde) Tanımlanabilir ama işlem yapılamaz
+        // Doğru int x = 5;
+        // Yanlış x = 5;
+        //Burada Tanımlanan Değişkenler Her metodun içinde kullanılabilir
         static void Main(string[] args)
         {
+            //işlemler buraya yazılacak
+            //     VeriTürü veriİsmi = VeriDeğeri
+            //Örnek  string   isim   =  "Erkan";
+            // string Sadece Metin değer alır
+            // int tam sayı alır
         }
     }
 }
